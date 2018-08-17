dc0e8e290c857c1678b62c99db8946a6a4537014
ashrawan <shrawan.adhikari.31@gmail.com>
2018-08-17 13:01:12 +0545

    test and dev profile for database

--- src/main/java/com/demo/myapp/controller/UserController.java
+++ src/main/java/com/demo/myapp/controller/UserController.java
@@ -19,54 +19,64 @@ import com.demo.myapp.service.UserService;
 @RestController
 public class UserController {
 
-    private UserService userService;
+	private UserService userService;
 
-    public UserController(final UserService userService) {
-        this.userService = userService;
-    }
+	public UserController(final UserService userService) {
+		this.userService = userService;
+	}
 
-    @GetMapping("/users")
-    public ResponseEntity<List<User>> retrieveAllUsers() {
+	@GetMapping("/users")
+	public ResponseEntity<List<User>> retrieveAllUsers() {
 
-        List<User> userList = userService.getAllUsers();
-        if(userList==null || userList.size()==0 ){
-            return new ResponseEntity(userList,HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<List<User>>(userList, HttpStatus.OK);
-    }
+		List<User> userList = userService.getAllUsers();
+		if (userList == null || userList.size() == 0) {
+			return new ResponseEntity(userList, HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<List<User>>(userList, HttpStatus.OK);
+	}
 
-    @PostMapping("/users")
-    public ResponseEntity<User> createUser(@RequestBody User user) {
+	@GetMapping("/users/{id}")
+	public ResponseEntity<User> getUserById(@PathVariable long id) {
 
-        User userRe = userService.addUser(user);
-        if(userRe == null ){
-            return new ResponseEntity<>(HttpStatus.CONFLICT);
-        }
-        return new ResponseEntity<User>(userRe, HttpStatus.OK);
+		User user = userService.getUser(id);
+		if (user == null) {
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<User>(user, HttpStatus.OK);
+	}
 
-    }
+	@PostMapping("/users")
+	public ResponseEntity<User> createUser(@RequestBody User user) {
 
-    @PutMapping("/users/{id}")
-    public ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable long id) {
-        user.setId(id);
-        User u = userService.updateUser(user);
-        if(u == null){
-            return new ResponseEntity<>(HttpStatus.CONFLICT);
-        }
-        return new ResponseEntity<User>(u, HttpStatus.OK);
+		User userRe = userService.addUser(user);
+		if (userRe == null) {
+			return new ResponseEntity<>(HttpStatus.CONFLICT);
+		}
+		return new ResponseEntity<User>(userRe, HttpStatus.OK);
 
-    }
+	}
 
-    @DeleteMapping("/users/{id}")
-    public ResponseEntity deleteUser(@PathVariable int id) {
+	@PutMapping("/users/{id}")
+	public ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable long id) {
+		user.setId(id);
+		User u = userService.updateUser(user);
+		if (u == null) {
+			return new ResponseEntity<>(HttpStatus.CONFLICT);
+		}
+		return new ResponseEntity<User>(u, HttpStatus.OK);
 
-        try {
-            userService.deleteUser(id);
-        } catch (Exception e) {
-            return new ResponseEntity(HttpStatus.CONFLICT);
-        }
+	}
 
-        return new ResponseEntity(HttpStatus.OK);
-    }
+	@DeleteMapping("/users/{id}")
+	public ResponseEntity deleteUser(@PathVariable int id) {
+
+		try {
+			userService.deleteUser(id);
+		} catch (Exception e) {
+			return new ResponseEntity(HttpStatus.CONFLICT);
+		}
+
+		return new ResponseEntity(HttpStatus.OK);
+	}
 
 }
--- src/test/java/com/demo/myapp/dao/TestUserDao.java
+++ src/test/java/com/demo/myapp/dao/TestUserDao.java
@@ -9,12 +9,15 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.demo.myapp.model.User;
 
+@ActiveProfiles("test")
 @ContextConfiguration("classpath:dispatcher-servlet.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
 public class TestUserDao {
